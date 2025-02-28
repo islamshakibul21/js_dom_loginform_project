05581dc6fc40b5adba41e4d7b15a9f67c4de8b4a
shakibul Islam <shakibulislam1962@gmail.com>
2025-02-28 22:22:57 +0600

    full_complet_selection_option_others_button

---
+++ javascript/addmoney.js
@@ -0,0 +1,19 @@
+  
+  document.getElementById("btn-add").addEventListener('click',function(event){
+
+    event.preventDefault;
+        const amount=document.getElementById('amount').value;
+        const pin=document.getElementById('pin').value;
+
+
+         const convertPin=parseInt(pin) ;
+         const convertAmount=parseFloat(amount);
+         if(convertPin === 1234){
+             if()
+            
+        // }
+        // else{
+        //     console.log('pin is not correct');
+        // }
+  })
+
