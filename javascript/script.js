13ec9d18490430658d47bcaee0dd00942ae62818
shakibul Islam <shakibulislam1962@gmail.com>
2025-02-28 11:47:54 +0600

    hero_section_card

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+ 
+ document.getElementById('login-btn')
+ .addEventListener('click',function(event){
+    event.preventDefault();
+    const accountNum=document.getElementById("input-Num").value;
+    const pinNum = document.getElementById("input").value;
+     const convertNum = parseInt(pinNum);
+     if(accountNum.length === 11){
+        if(convertNum===1234){
+           window.location.href="../main.html";
+        }
+        else{
+            console.log('pin not correct');
+            
+        }
+     }
+     else{
+        console.log('number is not correct');
+     }
+   
+   
+ })
+
